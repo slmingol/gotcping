523f84cb181cf3af9aab2eeca6a07b74c8b8739f
Pedro Perez <pepere@microsoft.com>
2016-09-28 14:36:06 +0100

    Moved from args to flags

--- main.go
+++ main.go
@@ -1,50 +1,49 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "net"
-  "flag"
-  "time"
-  "strconv"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
 )
 
-func usage(filename string) {
-  fmt.Println(fmt.Sprintf("Usage: %s [-t timeout_second] <host> <port>", filename))
-}
-
 func main() {
-  filename := os.Args[0]
 
-  var timeout = flag.Int("t", 10, "timeout second")
-  flag.Parse()
-  args := flag.Args()
-  if len(args) < 2 {
-    fmt.Println("Argument Miss")
-    usage(filename)
-    os.Exit(2)
-  }
+	hostPtr := flag.String("host", "bing.com", "Host or IP address to test")
+	portPtr := flag.Int("port", 80, "Port number to query")
+	//countPtr := flag.Int("count", 10, "Number of requests to send")
+	timeoutPtr := flag.Int("timeout", 5, "Timeout for each request, in seconds")
 
-  port, err := strconv.Atoi(args[1])
-  if err != nil || port < 1 || port > 65535 {
-    fmt.Println(fmt.Sprintf("Argument [%s] was not correct, <port> must be a positive integer in the range 1 - 65535", args[1]))
-    os.Exit(2)
-  }
+	flag.Parse()
 
-  host := args[0]
-  _, err = net.LookupIP(host)
-  if err != nil {
-    fmt.Println("error: unknown host")
-    os.Exit(2)
-  }
+	//args := flag.Args()
+	host := *hostPtr
+	port := *portPtr
+	//count := *countPtr
+	timeout := *timeoutPtr
 
-  addr := fmt.Sprintf("%s:%d", host, port)
+	/*	port, err := strconv.Atoi(args[1])
+		if err != nil || port < 1 || port > 65535 {
+			fmt.Println(fmt.Sprintf("Argument [%s] was not correct, <port> must be a positive integer in the range 1 - 65535", args[1]))
+			os.Exit(2)
+		}*/
 
-  _, err = net.DialTimeout("tcp", addr, time.Second * time.Duration(*timeout))
-  if err != nil {
-    fmt.Println(fmt.Sprintf("%s port %d closed.", host, port))
-    os.Exit(0)
-  }
+	_, err := net.LookupIP(host)
+	if err != nil {
+		fmt.Println("error: unknown host")
+		os.Exit(2)
+	}
 
-  fmt.Println(fmt.Sprintf("%s port %d open.", host, port))
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	_, err = net.DialTimeout("tcp", addr, time.Second*time.Duration(timeout))
+	if err != nil {
+		fmt.Println(fmt.Sprintf("%s port %d closed.", host, port))
+		os.Exit(0)
+	}
+
+	fmt.Println(fmt.Sprintf("%s port %d open.", host, port))
 }
+
+// func ping(host, port, count, timeout) {}
