355d2ed66494e107d54244f3cecb1e529585e29d
Pedro Perez <pepere@microsoft.com>
2016-09-30 13:59:03 +0100

    Added looping, RTT measured, lots of bugs

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	hostPtr := flag.String("host", "bing.com", "Host or IP address to test")
 	portPtr := flag.Int("port", 80, "Port number to query")
-	//countPtr := flag.Int("count", 10, "Number of requests to send")
+	countPtr := flag.Int("count", 10, "Number of requests to send")
 	timeoutPtr := flag.Int("timeout", 5, "Timeout for each request, in seconds")
 
 	flag.Parse()
@@ -20,14 +20,8 @@ func main() {
 	//args := flag.Args()
 	host := *hostPtr
 	port := *portPtr
-	//count := *countPtr
+	count := *countPtr
 	timeout := *timeoutPtr
-
-	/*	port, err := strconv.Atoi(args[1])
-		if err != nil || port < 1 || port > 65535 {
-			fmt.Println(fmt.Sprintf("Argument [%s] was not correct, <port> must be a positive integer in the range 1 - 65535", args[1]))
-			os.Exit(2)
-		}*/
 
 	_, err := net.LookupIP(host)
 	if err != nil {
@@ -35,15 +29,33 @@ func main() {
 		os.Exit(2)
 	}
 
-	addr := fmt.Sprintf("%s:%d", host, port)
+	/*addr := fmt.Sprintf("%s:%d", host, port)*/
 
-	_, err = net.DialTimeout("tcp", addr, time.Second*time.Duration(timeout))
-	if err != nil {
-		fmt.Println(fmt.Sprintf("%s port %d closed.", host, port))
-		os.Exit(0)
-	}
+	ping(host, port, count, timeout)
 
-	fmt.Println(fmt.Sprintf("%s port %d open.", host, port))
+	/*	_, err = net.DialTimeout("tcp", addr, time.Second*time.Duration(timeout))
+		if err != nil {
+			fmt.Println(fmt.Sprintf("%s port %d closed.", host, port))
+			os.Exit(0)
+		}
+
+		fmt.Println(fmt.Sprintf("%s port %d open.", host, port))*/
 }
 
-// func ping(host, port, count, timeout) {}
+func ping(host string, port int, count int, timeout int) {
+
+	addr := fmt.Sprintf("%s:%d", host, port)
+
+	for i := 1; count >= i; i++ {
+		timeStart := time.Now()
+		_, err := net.DialTimeout("tcp", addr, time.Second*time.Duration(timeout))
+		responseTime := time.Since(timeStart)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("%s port %d closed.", host, port))
+			os.Exit(0)
+		}
+
+		fmt.Println(fmt.Sprintf("Connected to %s:%d, RTT=%.2fms", host, port, float32(responseTime)/1e6))
+		time.Sleep(1e9)
+	}
+}
